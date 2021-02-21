bad443a557dd28451a01d9658562c7cba8b57543
Varun Patil <radialapps@gmail.com>
2021-02-22 03:21:30 +0530

    index: remove double export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ export * from './typings';
 export * from './version-vector';
 export * from './socket-base';
 export * from './socket';
-export * from './socket-base';
 export * from './socket-shared';
 export * from './store-memory';
